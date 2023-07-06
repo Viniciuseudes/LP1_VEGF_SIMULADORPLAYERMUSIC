b6dd0a5489ccaf999a16ea40035aa72e161d1b1b
Vinicius Eudes <viniciuseudes@MacBook-Air-de-Vinicius.local>
2023-07-06 19:37:28 -0300

    Envio de Arquivos

---
+++ usuario.h
@@ -0,0 +1,26 @@
+#ifndef PROJ_SPOTIFY_USUARIO_H
+#define PROJ_SPOTIFY_USUARIO_H
+
+#include <string>
+#include <vector>
+
+
+class Usuario
+{
+
+public:
+    stn::string Getnome() const;
+    std::string GetDataNasc() const;
+    std::string GetNacionalidade() const;
+    std::string GetEmail() const;
+
+
+private:
+    std::string nome;
+    std::string datanasc;
+    std::string nacionalidade;
+    std::string email;
+};
+
+
+#endif
